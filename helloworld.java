af6cc8b296f1ca3d8a987ba2945ae67e1a06a075
rushtoakshay <110481066+rushtoakshay@users.noreply.github.com>
2022-08-04 18:08:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - aryan triggered by webhook3");
+    System.out.println("Hello World - akshay triggered by webhook3");
     }
 }
